87a272ee2d09431b7159275e99caca806d5666a4
Yasin Shuman <email@yasin.io>
2018-08-09 22:18:46 -0700

    saving progress: FE built, can pull up list of books so far

---
+++ client/src/components/BookList.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
+
+const getBooksQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+class BookList extends Component {
+  displayBooks() {
+    const data = this.props.data;
+    if (data.loading) {
+      return <div>Loading Books</div>;
+    } else {
+      return data.books.map(book => {
+        return <li key={book.id}>{book.name}</li>;
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <ul id="book-list">{this.displayBooks()}</ul>
+      </div>
+    );
+  }
+}
+
+export default graphql(getBooksQuery)(BookList);
--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 mongoose.connect(
   "mongodb://yasin:test123@ds235418.mlab.com:35418/graphql-fun",
