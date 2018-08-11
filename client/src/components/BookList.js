b1de5fad4373d4f862d4de6d5a342588a4ee0ed8
Yasin Shuman <email@yasin.io>
2018-08-11 02:36:59 -0700

    app works lulz

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,15 +1,32 @@
 import React, { Component } from "react";
 import { graphql } from "react-apollo";
 import { getBooksQuery } from "../queries/queries";
+import BookDetails from "./BookDetails";
 
 class BookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    };
+  }
+
   displayBooks() {
     const data = this.props.data;
     if (data.loading) {
       return <div>Loading Books</div>;
     } else {
       return data.books.map(book => {
-        return <li key={book.id}>{book.name}</li>;
+        return (
+          <li
+            key={book.id}
+            onClick={event => {
+              this.setState({ selected: book.id });
+            }}
+          >
+            {book.name}
+          </li>
+        );
       });
     }
   }
@@ -18,6 +35,7 @@ class BookList extends Component {
     return (
       <div>
         <ul id="book-list">{this.displayBooks()}</ul>
+        <BookDetails bookId={this.state.selected} />
       </div>
     );
   }
